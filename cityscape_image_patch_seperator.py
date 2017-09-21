e0b3447a6d073711f249b1423fa64425aa3b2ec3
dimimal <dimimallios@gmail.com>
2017-09-21 16:24:34 +0300

    fixing dataset structure

--- cityscape_image_patch_seperator.py
+++ cityscape_image_patch_seperator.py
@@ -2,12 +2,12 @@
 
 import sys
 import os
-import re
 from skimage import io
 import numpy as np
+import labels
 
 # How many images you want to cut into patches
-imageSet = 100
+imageSet = 1
 
 global patchSize
 
@@ -15,17 +15,44 @@ patchSize = 35
 rawImagePattern = 'leftImg8bit.png'
 fineAnnotPattern = 'labelTrainIds.png'
 
-# Add the file paths
-imagePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/train/aachen'
-fineAnnotPath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/train/aachen'
-outTrainPath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/train_patches'
-outAnnotPath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/train_annot_patches'
+#####################################################
+# Configure paths for leftImg8bit image set
+#####################################################
+
+# imagePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/train/aachen'
+# Train set Paths
+trainImagePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/train'
+outTrainImgPath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/train_set'
+
+# Validation set Paths
+valImagePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/validation'
+outValImgPath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/validation_set'
+
+# Test set Paths
+testImagePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/test'
+outTestImgPath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/leftImg8bit/test_set'
+
+######################################################
+# gtFine Paths
+######################################################
+trainFinePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/train'
+outTrainFinePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/train_set'
+
+valFinePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/validation'
+outValFinePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/validation_set'
+
+testFinePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/test'
+outTestFinePath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/test_set'
+
+# labelTrainIds 
+#fineAnnotPath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/train/aachen'
+#outAnnotPath = '/media/dimitris/TOSHIBA EXT/UTH/Thesis/Cityscapes_dataset/gtFine/train_annot_patches'
 
 #############################################
 # Extracts the patches from the image and 
 # saves to path
 #############################################
-def imagePatchExtractor(image, file, path):
+def imagePatchExtractor(image, file, imagepath, finepath):
 	counter = 0
 	index = 0
 	print file
@@ -36,25 +63,24 @@ def imagePatchExtractor(image, file, path):
 
 	while i < h :
 		while j < w:
+			# Check boundaries
 			if j > w or j > w-patchSize:
 				croppedImage = image[i:i+patchSize,w-patchSize::,:]
 			else:
 				croppedImage = image[i:i+patchSize,j:j+patchSize,:]
 
-			if re.search(rawImagePattern, file, flags=0):
-				rename = file.replace(rawImagePattern, str(counter)+'_'+rawImagePattern)
-				io.imsave(outTrainPath+'/'+rename, croppedImage)
-			elif re.search(fineAnnotPattern, file, flags=0):
-				rename = file.replace(fineAnnotPattern, str(counter)+'_'+fineAnnotPattern)
-				io.imsave(outAnnotPath+'/'+rename, croppedImage)
-			
+			rename = file.replace(rawImagePattern, str(counter)+'_'+rawImagePattern)
+			io.imsave(outTrainPath+'/'+rename, croppedImage)
+						
 			j += patchSize
 			counter += 1
 		
 		i += patchSize
+		# Leave the last row pixels (max 35 pixels abandoned)
 		if i > h-patchSize:
 			return
 		j = 0
+
 
 ######################################################
 # Extracts the patches from TrainLabelIds images
@@ -77,12 +103,10 @@ def trainLabelsPatchExtractor(image, file, path):
 			else:
 				croppedImage = image[i:i+patchSize,j:j+patchSize]
 
-			if re.search(rawImagePattern, file, flags=0):
-				rename = file.replace(rawImagePattern, str(counter)+'_'+rawImagePattern)
-				io.imsave(outTrainPath+'/'+rename, croppedImage)
-			elif re.search(fineAnnotPattern, file, flags=0):
-				rename = file.replace(fineAnnotPattern, str(counter)+'_'+fineAnnotPattern)
-				io.imsave(outAnnotPath+'/'+rename, croppedImage)
+			centerLabel = croppedImage[patchSize/2,patchSize/2]
+			
+			rename = file.replace(fineAnnotPattern, str(counter)+'_'+'L_'+ '{0:03d}'.format(centerLabel) +'_'+fineAnnotPattern )
+			io.imsave(outAnnotPath+'/'+rename, croppedImage)
 			
 			j += patchSize
 			counter += 1
@@ -92,22 +116,69 @@ def trainLabelsPatchExtractor(image, file, path):
 			return
 		j = 0
 
+############################################################
+# Checks if the folders for our extracted patches of images
+# are in place, each folder(train,val,test) has 19 folders
+# one of each class
+###########################################################
+def folderCheck():
+	for classFolder in labels.listLabels:
+		if classFolder not in os.listdir(outTrainPath):
+			os.mkdir(classFolder)
+		if classFolder not in os.listdir(outValPath):
+			os.mkdir(classFolder)
+		if classFolder not in os.listdir(outTestPath):
+			os.mkdir(classFolder)
+		if classFolder not in os.listdir(outTrainFinePath):
+			os.mkdir(classFolder)
+		if classFolder not in os.listdir(outValFinePath):
+			os.mkdir(classFolder)
+		if classFolder not in os.listdir(outTestFinePath):
+			os.mkdir(classFolder)
+
 
 ##########################################
 # Main function 
 ##########################################
 def main():
+	
+
+	# Check if the folders for each class are in place
+	folderCheck()
+	
+
+	
 	# Extract Raw images
-	'''
 	counter = 0
-	for file in sorted(os.listdir(imagePath)):
-		print file
-		if counter == imageSet:
-			break
-		image = io.imread(imagePath+'/'+file)
-		#image = image/255.0
-		imagePatchExtractor(image, file, imagePath)
-		counter += 1
+	for city in sorted(os.listdir(trainImagePath)):
+		for file in city:
+			if counter == imageSet:
+				break
+			image = io.imread(trainImagePath+'/'+city+'/'+file)
+			#image = image/255.0
+			imagePatchExtractor(image, file, trainImagePath, trainFinePath)
+			counter += 1
+	
+	counter = 0
+	for city in sorted(os.listdir(valImagePath)):
+		for file in city:
+			if counter == imageSet:
+				break
+			image = io.imread(valImagePath+'/'+city+'/'+file)
+			#image = image/255.0
+			imagePatchExtractor(image, file, valImagePath, valFinePath)
+			counter += 1
+
+	counter = 0
+	for city in sorted(os.listdir(testImagePath)):
+		for file in city:
+			if counter == imageSet:
+				break
+			image = io.imread(testImagePath+'/'+city+'/'+file)
+			#image = image/255.0
+			imagePatchExtractor(image, file, testImagePath, testFinePath)
+			counter += 1
+
 	'''
 	# Extract Label images
 	counter = 0
@@ -118,8 +189,8 @@ def main():
 			continue
 		image = io.imread(fineAnnotPath+'/'+file)
 		#image = image/255.0 
-		trainLabelsPatchExtractor(image,file, fineAnnotPath)
+		trainLabelsPatchExtractor(image, file, fineAnnotPath)
 		counter += 1
-
+	'''
 if __name__ == '__main__':
 	main()
---
+++ labels.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+# This script has the labels for our dataset
+# labels 		: 	 {class : id}     access by its class name 
+# idlabels 		:    {id : class}   access by its label id
+# listLabels	:    A list with the class labels with the same order
+
+listLabels = [
+			'road'			   	   , 
+			'sidewalk'			   ,
+			'building'             ,
+			'wall'                 ,
+			'fence'                ,
+			'pole'                 ,
+			'traffic light'        ,
+			'traffic sign'         ,
+			'vegetation'           ,
+			'terrain'              ,
+			'sky'                  ,
+			'person'               ,
+			'rider'                ,
+			'car'                  ,
+			'truck'                ,
+			'bus'  				   ,
+			'train'				   ,
+			'motorcycle'		   ,
+			'bicycle' 
+			]
+
+labels = {
+			'road'				   :	0    , 
+			'sidewalk'			   :	1    ,
+			'building'             :    2    ,
+			'wall'                 :    3    ,
+			'fence'                :    4    ,
+			'pole'                 :    5    ,
+			'traffic light'        :    6    ,
+			'traffic sign'         :    7    ,
+			'vegetation'           :    8    ,
+			'terrain'              :    9    ,
+			'sky'                  :   10    ,
+			'person'               :   11    ,
+			'rider'                :   12    ,
+			'car'                  :   13    ,
+			'truck'                :   14    ,
+			'bus'                  :   15    ,
+			'train'                :   16    ,
+			'motorcycle'           :   17    ,
+			'bicycle'              :   18
+		}
+
+idLabels = { id:label for label,id in labels.iteritems()}
