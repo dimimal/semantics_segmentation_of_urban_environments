df53e8892ad645ae2a3ea437f30e9f87a6a67c5e
dimimal <dimimallios@gmail.com>
2018-01-17 22:24:38 +0200

    add toQImage Function

--- modelViewer/modelViewer.py
+++ modelViewer/modelViewer.py
@@ -876,7 +876,25 @@ class CityscapesViewer(QtGui.QMainWindow):
         qp.restore()
 
         return overlay
+    
+    # Numpy array to QImage function
+    def toQImage(im, copy=False):
+        if im is None:
+            return QImage()
 
+        if im.dtype == np.uint8:
+            if len(im.shape) == 2:
+                qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_Indexed8)
+                qim.setColorTable(gray_color_table)
+                return qim.copy() if copy else qim
+
+            elif len(im.shape) == 3:
+                if im.shape[2] == 3:
+                    qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_RGB888);
+                    return qim.copy() if copy else qim
+                elif im.shape[2] == 4:
+                    qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_ARGB32);
+                    return qim.copy() if copy else qim
 
     # Draw the label name next to the mouse
     def drawLabelAtMouse(self, qp):
